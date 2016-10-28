f9663850351f16e5e5b0a8832b7ff854a253bb98
DergunK <magi_renn@mail.ru>
2016-10-28 20:10:00 +0300

    Delete string.h

--- HT_PART2/inc/string.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STRING_H__
-#define __STRING_H__
-
-#include <stddef.h>
-
-size_t strlen(const char *str);
-void *memcpy(void *dst, const void *src, size_t size);
-
-#endif /*__STRING_H__*/
